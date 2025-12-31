6bd735b5353f397ad5a17a648f1c4d4059f2e668
FoxVelocity <foxvelocity@laposte.net>
2025-12-31 14:33:40 +0100

    Tsx en Js

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Crucial pour GitHub Pages : génère des chemins relatifs
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 });
